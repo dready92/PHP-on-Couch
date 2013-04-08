1ecfc442f3e55eba298109dba3700c719a0595ad
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-04-08 16:16:38 +0200

    refresh class map file

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -5,16 +5,16 @@ if ( !defined('PHPONCOUCH_LIBRARY_PATH') ) {
 }
 
 return array(
-    'couch'                         => PHPONCOUCH_LIBRARY_PATH . 'couch.php',
-    'couchAdmin'                    => PHPONCOUCH_LIBRARY_PATH . 'couchAdmin.php',
-    'couchClient'                   => PHPONCOUCH_LIBRARY_PATH . 'couchClient.php',
-    'couchDocument'                 => PHPONCOUCH_LIBRARY_PATH . 'couchDocument.php',
-    'couchReplicator'               => PHPONCOUCH_LIBRARY_PATH . 'couchReplicator.php',
-    'couchException'                => PHPONCOUCH_LIBRARY_PATH . 'couchException.php',
-    'couchConflictException'        => PHPONCOUCH_LIBRARY_PATH . 'couchConflictException.php',
-    'couchExpectationException'     => PHPONCOUCH_LIBRARY_PATH . 'couchExpectationException.php',
-    'couchForbiddenException'       => PHPONCOUCH_LIBRARY_PATH . 'couchForbiddenException.php',
-    'couchNoResponseException'      => PHPONCOUCH_LIBRARY_PATH . 'couchNoResponseException.php',
-    'couchNotFoundException'        => PHPONCOUCH_LIBRARY_PATH . 'couchNotFoundException.php',
-    'couchUnauthorizedException'    => PHPONCOUCH_LIBRARY_PATH . 'couchUnauthorizedException.php',
+    'PhpOnCouch_AbstractClient'                     => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/AbstractClient.php',
+    'PhpOnCouch_Admin'                              => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Admin.php',
+    'PhpOnCouch_Client'                             => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Client.php',
+    'PhpOnCouch_Document'                           => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Document.php',
+    'PhpOnCouch_Replicator'                         => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Replicator.php',
+    'PhpOnCouch_Exception_Exception'                => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/Exception.php',
+    'PhpOnCouch_Exception_ConflictException'        => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/ConflictException.php',
+    'PhpOnCouch_Exception_ExpectationException'     => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/ExpectationException.php',
+    'PhpOnCouch_Exception_ForbiddenException'       => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/ForbiddenException.php',
+    'PhpOnCouch_Exception_NoResponseException'      => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/NoResponseException.php',
+    'PhpOnCouch_Exception_NotFoundException'        => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/NotFoundException.php',
+    'PhpOnCouch_Exception_UnauthorizedException'    => PHPONCOUCH_LIBRARY_PATH . '/PhpOnCouch/Exception/UnauthorizedException.php',
 );
