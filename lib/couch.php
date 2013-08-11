e84ee6393ce36e93e326dea091895028962f3323
Martin Kokeš <shr3k@typo3-hosting.com>
2013-08-12 01:07:37 +0200

    Supress PHP warnings when open_basedir i set or/and safe mode is on

--- lib/couch.php
+++ lib/couch.php
@@ -551,7 +551,7 @@ class couch {
 		$this->_curl_addCustomOptions ($http);
 		curl_setopt($http,CURLOPT_HEADER, true);
 		curl_setopt($http,CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($http,CURLOPT_FOLLOWLOCATION, true);
+		if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($http,CURLOPT_FOLLOWLOCATION, true);
 
 		$response = curl_exec($http);
 		curl_close($http);
@@ -590,7 +590,7 @@ class couch {
 		curl_setopt($http, CURLOPT_UPLOAD, true);
 		curl_setopt($http, CURLOPT_HEADER, true);
 		curl_setopt($http, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($http, CURLOPT_FOLLOWLOCATION, true);
+		if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($http, CURLOPT_FOLLOWLOCATION, true);
 		$fstream=fopen($file,'r');
 		curl_setopt($http, CURLOPT_INFILE, $fstream);
 		curl_setopt($http, CURLOPT_INFILESIZE, filesize($file));
@@ -631,7 +631,7 @@ class couch {
 		curl_setopt($http, CURLOPT_HTTPHEADER,$http_headers);
 		curl_setopt($http, CURLOPT_HEADER, true);
 		curl_setopt($http, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($http, CURLOPT_FOLLOWLOCATION, true);
+		if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) curl_setopt($http, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($http, CURLOPT_POSTFIELDS, $data);
 		$this->_curl_addCustomOptions ($http);
 		$response = curl_exec($http);
